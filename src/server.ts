c8f615c1a95ebaecbbb9b7c9b231f092032b5e80
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-07-09 10:26:40 -0300

    chore: configure fastify http framework

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('NLW Journey!')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'NLW Journey'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Server is running at http://localhost:3333/')
+})
